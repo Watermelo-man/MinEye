dc826f20fae2d6dff24b23ccdbfa5f5fe3c17ea5
Watermelo-man <watermelo.man@yandex.ru>
2023-03-06 02:09:31 +0300

    Заработала передача
    изображения по кнопке из контроллера

--- control.py
+++ control.py
@@ -3,22 +3,35 @@ from pathlib import Path
 import  universal_model 
 import cv2
 from PIL import Image
-
+from PyQt6.QtGui import QPixmap, QImage
 
 
 model = universal_model.kernel.kernel(model_path="C:\\Projects\\orereco\\ourmodels\\onlygold.pt", device="CUDA:0")
 
 #im = cv2.imread("C:\\Projects\\orereco\\source\\bottle.jpg")
 
-#im =Image.open("C:\\Projects\\orereco\\source\\0038.png")
+im =Image.open("C:\\Projects\\orereco\\source\\0038.png")
 
-im = cv2.VideoCapture(0)
+#im = cv2.VideoCapture(0)
 
-#md = universal_model.modelType.PictureModel(model.kernel)
-md = universal_model.modelType.VideoModel(model.kernel)
+md = universal_model.modelType.PictureModel(model.kernel)
+#md = universal_model.modelType.VideoModel(model.kernel)
 
 
 print(type(im))
+
+
+def analyseImg():
+    md.predict(im)
+    res= md.showLastShot()
+    #create from numpy Qpixmap
+    h,w,ch = res.shape
+    bytes_per_line = ch*w
+    convert_to_Qt_format = QImage(res.data, w, h, bytes_per_line, QImage.Format.Format_RGB888)
+    #create from numpy Qpixmap
+    return convert_to_Qt_format
+
+
 
 '''
 while(True):
--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import sys
 import cv2
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel , QMainWindow
+from PyQt6.QtCore import QSize, Qt, QObject, pyqtSignal
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel , QMainWindow, QPushButton,QVBoxLayout
 from PyQt6.QtGui import QPixmap, QImage
 from  control import *
 
@@ -11,9 +11,56 @@ class MainWindow(QMainWindow):
         super().__init__()
         #img = QPixmap("C:\\Users\\User\\Desktop\\orereco\\source\\0038.jpg") 
         self.setWindowTitle("Orereco")
+        '''
+        layout = QVBoxLayout()
+        widgets = [QPushButton,QLabel]
+
+        for w in widgets:
+            layout.addWidget(w())
+
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        self.setCentralWidget(widget)
+
+        '''
+
+
+        self.setFixedSize(QSize(900, 600))
+
+        button = QPushButton("Press")
+
+        self.setCentralWidget(button)
+        
+        self.img = QLabel("hello")
+
+        #img.resize(640,480)
+
+        #self.setCentralWidget(img)
+
+
+        #button.show()
+        
+        #img.show()
+
+
+        button.clicked.connect(self.the_button_was_clicked)
+
+    def the_button_was_clicked(self):
+        im = analyseImg()
+        self.img.setPixmap(QPixmap.fromImage(im).scaled(640,480))
+        self.setCentralWidget(self.img)
+        '''
         #self.setFixedSize(QSize(400, 300))
 
+        button = QPushButton("Call func")       
 
+
+        button.clicked.connect(self.the_button_was_clicked)
+
+
+        button.show()
+        
         labl = QLabel()
         md.predict(im)
         res= md.showLastShot()
@@ -30,9 +77,12 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(labl)
         labl.show()
+        
+ 
+    def the_button_was_clicked(self):
+            print("Clicked!")
+    '''
 
-        
-        
 app = QApplication(sys.argv)
 
 window = MainWindow()
