3d8eddd030d8cb8263e5582786214433565ab129
Watermelo-man <79632032+Watermelo-man@users.noreply.github.com>
2023-03-16 22:48:52 +0300

    working camera in lab

---
+++ control.py
@@ -0,0 +1,104 @@
+import sys
+from pathlib import Path
+import universal_model 
+import cv2
+from PIL import Image
+from PyQt6.QtGui import QPixmap, QImage
+from PyQt6.QtWidgets import QFileDialog
+from enum import Enum
+import os
+
+class types(Enum):
+    photo = 1
+    video = 2
+
+
+class controller():
+    model = None
+    source = None
+    kernel = None
+    def __init__(self):
+         self.kernel = universal_model.kernel.kernel(model_path="C:\\Users\\CNT\\Desktop\\ul_git_source\\ourmodels\\onlygold.pt", device="CPU")
+         
+    def selectType(self,type:int):
+        if type == 1:
+            self.model = universal_model.modelType.models.selectModel(self.kernel.kernel, "PictureModel")
+            return 0
+        elif type == 2:
+            self.model == universal_model.modelType.models.selectModel(self.kernel.kernel, "VideoModel")
+            return 0
+        else:
+            return -1
+    
+    def selectSource(self,src:int = 2):
+        if src == 1:
+            #self.source = Image.open("C:/Users/Alexander/Desktop/orereco/source/bottle.jpg")
+            path,check = QFileDialog.getOpenFileName(None, "Open Image","","Image Files (*.png *.jpg)")
+            self.source = Image.open(str(path))
+            return 0
+        elif src == 2:
+            print("we are here")
+            #self.source = cv2.VideoCapture(1)
+            sources='1'
+            sources = Path(sources).read_text().rsplit() if os.path.isfile(sources) else [sources]
+            n = len(sources)
+            #self.sources = [clean_str(x) for x in sources]  # clean source names for later
+            for i, s in enumerate(sources):  # index, source
+            # Start thread to read frames from video stream
+                st = f'{i + 1}/{n}: {s}... '
+            
+                
+                s = eval(s) if s.isnumeric() else s  # i.e. s = '0' local webcam
+            
+                cap = cv2.VideoCapture(s)
+                
+                if cap.isOpened():
+                    ret,frame = cap.read()
+                    cv2.imshow("cum",frame)
+            return 0
+        else:
+            return -1
+   
+    def analyseShot(self):#(model:universal_model.modelType.Imodel = md,im = im):
+        print(self.source)
+        self.model.predict(self.source)
+        res= self.model.showLastShot()
+        #create from numpy Qpixmap
+        h,w,ch = res.shape
+        bytes_per_line = ch*w
+        convert_to_Qt_format = QImage(res.data, w, h, bytes_per_line, QImage.Format.Format_RGB888)
+        #create from numpy Qpixmap
+        return convert_to_Qt_format
+
+
+cont = controller()
+cont.selectType(2)
+#cont.selectSource()
+
+
+
+
+
+
+'''
+while(True):
+    
+
+    md.predict(im)
+
+    res = md.showLastShot()
+
+
+    cv2.imshow("lol",cv2.resize(res,[640,480]))#cv2.resize(res,[640,480]))
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+#cv2.waitKey(0)
+'''
+
+"""
+from universal_model import kernel
+from universal_model import modelType
+
+"""
+ 
--- gui.py
+++ gui.py
@@ -1,43 +1,62 @@
-from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QApplication, QMainWindow
 import sys
+import cv2
+from PyQt6.QtCore import QSize, Qt, QObject, pyqtSignal
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel , QMainWindow, QPushButton,QGridLayout
+from PyQt6.QtGui import QPixmap, QImage
 
-    # caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, "C:/Projects/orereco/universal_model")
-
-#from  universal_model.universal_model import *
-
-
-
-# Только для доступа к аргументам командной строки
-
-# Приложению нужен один (и только один) экземпляр QApplication.
-# Передаём sys.argv, чтобы разрешить аргументы командной строки для приложения.
-# Если не будете использовать аргументы командной строки, QApplication([]) тоже работает
-
-
-
-#Kernel = kernel("ourmodels/onlygold.pt","cuda:0")
-
+from  control import *
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow,self).__init__()
-        self.setGeometry(400,400,500,300)
-        #self.setWindowTitle("My App")
-        videowindow = QtWidgets.QLabel(self)
-        videowindow.setText("GUI application with PyQt5")
-        videowindow.show()
+        super().__init__()
 
+        self.setWindowTitle("Orereco")
+
+        self.setFixedSize(QSize(900, 600))
+
+        layout = QGridLayout()
+
+        button = QPushButton("Work")
+        
+        button2 = QPushButton("OpenFile")
+
+        button.clicked.connect(self.analyse)
+        button2.clicked.connect(self.open)
+
+        #self.setCentralWidget(button)
+        
+        self.img = QLabel("hello")
+
+        
+
+        
+
+        layout.addWidget(button,0,0)
+        layout.addWidget(self.img,0,1)
+        layout.addWidget(button2,1,0)
+
+
+
+
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def analyse(self):
+        im = cont.analyseShot()
+        self.img.setPixmap(QPixmap.fromImage(im).scaled(640,480))
+        #self.setCentralWidget(self.img)
+        self.img.show()
+
+    def open(self):
+        cont.selectSource()
+        
 
 app = QApplication(sys.argv)
 
-# Создаём виджет Qt — окно.
 window = MainWindow()
-window.show()  # Важно: окно по умолчанию скрыто.
+window.show()
 
-# Запускаем цикл событий.
 app.exec()
-
-
