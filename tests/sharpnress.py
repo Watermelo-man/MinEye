0c8094af270f63dec016b5bd0c892822fcb9e4bc
Watermelon <pipapopa@mail.com>
2023-09-08 17:09:16 +0300

    add sharpnwss test

---
+++ tests/sharpnress.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('source/bottle.jpg')
+kernel = np.array([[-1,-1,-1], [-1, 9.85,-1],[-1,-1,-1]])
+sharpened = cv2.filter2D(image, -1, kernel) # applying the sharpening kernel to the input image & displaying it.
+cv2.imshow('Image Sharpening', sharpened)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
